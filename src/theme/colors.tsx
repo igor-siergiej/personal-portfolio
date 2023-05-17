176ee00166ce5db4aa32b941f24da8ad84a5699e
Igor Siergiej <igorsiergiej@gmail.com>
2023-05-17 15:44:20 +0100

    Final theme rework!!

--- src/theme/colors.tsx
+++ src/theme/colors.tsx
@@ -1,47 +1,40 @@
-import variables from '../index.scss'
+import darkThemeColors from '../theme/darkTheme.scss'
+import lightThemeColors from '../theme/lightTheme.scss'
 
-enum ThemeType {
-    primary = '--primary',
-    secondary = '--secondary',
-    tertiary = '--tertiary',
-    background = '--background',
-	alternativeBackground = '--alternativeBackground'
+interface Theme {
+    primaryColor: String,
+    secondaryColor: String,
+    tertiaryColor: String,
+    backgroundColor: String,
+	alternativeBackground: String,
+	fontColor: String
 }
 
-const defaultTheme = {
-	primary: variables.primaryColor,
-	secondary: variables.secondaryColor,
-	tertiary: variables.tertiaryColor,
-	background: variables.backgroundColor,
-	alternativeBackground: variables.alternativeBackground
+const darkTheme: Theme = {
+	primaryColor: darkThemeColors.primaryColor,
+	secondaryColor: darkThemeColors.secondaryColor,
+	tertiaryColor: darkThemeColors.tertiaryColor,
+	backgroundColor: darkThemeColors.backgroundColor,
+	alternativeBackground: darkThemeColors.alternativeBackground,
+	fontColor: darkThemeColors.fontColor
 }
 
-type HSLA = {
-	h: number,
-	l: number,
-	s: number,
-	a: number
-}
-
-const alternativeTheme = {
-	primary: { h: 211, s: 100, l: 50, a: 1 } as HSLA,
-	secondary: { h: 31, s: 100, l: 50, a: 1 } as HSLA,
-	tertiary: { h: 312, s: 100, l: 50, a: 1 } as HSLA,
-	background: { h: 0, s: 0, l: 100, a: 1 } as HSLA,
-	alternativeBack: { h: 0, s: 0, l: 80, a: 1 } as HSLA
+const whiteTheme: Theme = {
+	primaryColor: lightThemeColors.primaryColor,
+	secondaryColor: lightThemeColors.secondaryColor,
+	tertiaryColor: lightThemeColors.tertiaryColor,
+	backgroundColor: lightThemeColors.backgroundColor,
+	alternativeBackground: lightThemeColors.alternativeBackground,
+	fontColor: lightThemeColors.fontColor
 }
 
 function changeTheme () {
-	for (let i = 0; i < Object.keys(ThemeType).length; i++) {
-		const colorName: string = Object.values(ThemeType)[i]
-
-		const newHLSA :HSLA = Object.values(alternativeTheme)[i]
-
-		document.documentElement.style.setProperty(colorName + '-h', newHLSA.h.toString() + 'deg')
-		document.documentElement.style.setProperty(colorName + '-l', newHLSA.l.toString() + '%')
-		document.documentElement.style.setProperty(colorName + '-s', newHLSA.s.toString() + '%')
-		document.documentElement.style.setProperty(colorName + '-a', newHLSA.a.toString())
+	const themeColors = Object.keys(whiteTheme)
+	const colorValues = Object.values(whiteTheme)
+	for (let i = 0; i < themeColors.length; i++) {
+		console.log(colorValues[i])
+		document.documentElement.style.setProperty('--' + themeColors[i], colorValues[i])
 	}
 }
 
-export { defaultTheme, ThemeType, changeTheme }
+export { changeTheme }
