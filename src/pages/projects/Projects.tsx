57b495fe0f46bf552b0569dc4dea09e8b42ac22c
Igor Siergiej <igorsiergiej@gmail.com>
2023-05-18 23:45:03 +0100

    missed out in commit

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import styles from './Projects.module.scss'
 import Pane from '../../components/pane/Pane'
+import Project from '../../components/project/Project'
+import styles from './Projects.module.scss'
+
+import dietPlanner from '../../images/projects/dietPlanner.png'
+import polyLingo from '../../images/projects/polyLingo.png'
 
 function Projects () {
 	return (
 		<div>
 			<Pane>
-			Projects
+				<h1> These are some of my projects:</h1>
+				<div className={styles.container}>
+					<div className={styles.projectsContainer}>
+						{Project(
+							dietPlanner,
+							'Diet Planner',
+							'Diet planning desktop app',
+							'https://github.com/igor-siergiej/diet-planner')
+						}
+
+						{Project(
+							polyLingo,
+							'PolyLingo',
+							'Language learning android app',
+							'https://github.com/igor-siergiej/PolyLingo')
+						}
+
+					</div>
+				</div>
 			</Pane>
 		</div>
 	)
